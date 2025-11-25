7284861322d7fac113ceeb4bfbabe8c3096c7cf5
Michael Gorman <michael.gorman@appius.com>
2025-11-25 11:32:16 +0000

    Test 002

--- imdb-competitive-extension/content.js
+++ imdb-competitive-extension/content.js
@@ -3,7 +3,7 @@
 
 const FIREBASE_DB_URL = "https://imdb-game-343f1-default-rtdb.firebaseio.com"; // Corrected URL
 const GAMES_ROOT = `${FIREBASE_DB_URL}/games`;
-
+//THIS IS A TEST
 // ----------------------
 // Actor list (same as previous)
 const actorList = [
